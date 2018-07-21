a656a239376f4cea442181050daaba8c2883a84a
stephen <stephen@farver.io>
2018-07-21 04:09:29 -0500

    first commit -- all tests passing

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  attr_reader :name
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.all
+    @@all.each {|dog_inst| puts dog_inst.name}
+  end
+end
